269c30df53e27d6c1ccba5f15ed81e64fb5be5f7
Connameg <connameg@users.noreply.github.com>
2015-10-04 08:20:26 -0500

    simplify code in letterCapitalize

--- CB_LetterCapitalize.js
+++ CB_LetterCapitalize.js
@@ -5,25 +5,14 @@ Objective:
 Capitalize the first letter of each word in a string.
 */
 
-function LetterCapitalize(str) {
-	newString = "";                                                  //declare variable to hold new string
-	var splitMe = str.split(" ");                                    //split str into array for easier searching
-		for (var i = 0; i < splitMe.length; i++) {                   //loop through each item in the array
-			for (var j = 0; j < splitMe[i].length; j++) {            //loop through each character in each item in the array
-				if (j === 0) {                                       //if j is 0 (the first letter at array index i),
-					if (splitMe[i].length === 1) {                   //if the word at [i] is only one letter long,
-						newString += splitMe[i].charAt(j).toUpperCase() + " ";   //add to newString with a space at the end
-					} else {                                          //otherwise,
-					newString += splitMe[i].charAt(j).toUpperCase(); //make it uppercase and add to newString
-				}
-				} else if (j === splitMe[i].length - 1) {            //if j is the last letter of the word at index i,
-					newString += splitMe[i].charAt(j) + " ";         //add the letter plus a space to newString
-				} else {                                             //otherwise,
-					newString += splitMe[i].charAt(j);               //add the letter to newString
-				}
-			}
-		}
-	return newString;
+function LetterCapitalize(str) { 
+var words = str.split(" ");                        //split into array
+var arr = [];
+  for (var i = 0; i < words.length; i++) {         //for each word in the array,
+    //push the capitalized first letter & the remaining letters into arr
+    arr.push(words[i].charAt(0).toUpperCase() + words[i].slice(1));
+  }
+return arr.join(" ");                              //join arr into string
 }
 
-LetterCapitalize("i ran there");                                     //call the function
+LetterCapitalize("i ran there");                   //call the function
