28e4b80bdd0bc2965195051b2bffeb4031acc15a
Connameg <connameg@users.noreply.github.com>
2015-10-04 07:44:13 -0500

    letterChanges code edit

--- CB_LetterChanges.js
+++ CB_LetterChanges.js
@@ -9,34 +9,31 @@ the argument. Then, capitalize each vowel.
 function LetterChanges(str) {
 //array contains all letters from a to z
 var changeAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var newWord = "";                                         //holds results
-var i;                                                    //declare counter variable
-for (i = 0; i < str.length; i++) {                        //loop through each character in str
-	if (/[^a-z]/.test(str[i])) {                          //if character at i is something OTHER than a letter,
-		newWord += str[i];                                //add it to newWord
-	} else {                                              //otherwise,
-	for (j = 0; j < changeAlpha.length; j++) {            //loop through each item in changeAlpha array
-		//if the character is a letter and has a match in changeAlpha:
-		if (str[i] === changeAlpha[j]){
-			if (changeAlpha[j] === "z") {                 //if the letter is z, replace it with a
-				newWord += "a";
-			} else {                                      //for any other letter, replace it with the matching index in the array plus 1
-				newWord += changeAlpha[j + 1];
-			}
-		} 
-	} 
-	}
+var newWord = "";                                       //holds results
+var i;  var j;                                          //declare counter variables
+for (i = 0; i < str.length; i++) {                      //loop through each character in str
+  if (/[^a-z]/.test(str[i])) {                          //if character at i is something OTHER than a letter,
+    newWord += str[i];                                  //add it to newWord
+  } else {                                              //otherwise,
+    for (j = 0; j < changeAlpha.length; j++) {          //loop through each item in changeAlpha array
+//if the character is a letter and has a match in changeAlpha:
+      if (str[i] === changeAlpha[j]){
+        if (changeAlpha[j] === "z") {                   //if the letter is z, replace it with a
+          newWord += "a";
+        } else {                                        //for any other letter, replace it with next letter in array
+          newWord += changeAlpha[j + 1];
+        }
+      } 
+    } 
+  }
 }
-var capVowel = "";                                        //new variable holds final version of string
-	for (var k = 0; k < newWord.length; k++){             //loop through every character in newWord
-		                                                  //if the character at k is a vowel,
-		if (newWord[k] === "a" || newWord[k] === "e" || newWord[k] === "i" || newWord[k] === "o" || newWord[k] === "u") {
-			capVowel += newWord[k].toUpperCase();         //capitalize it and add it to capVowel
-		} else {
-			capVowel  += newWord[k];                      //otherwise add the character to capVowel
-		}
-	}
-return capVowel;                                          //return final version with capitalized vowels
+//replace all vowels with capitalized versions
+var fix = newWord.replace(/a/g, "A")
+.replace(/e/g, "E")
+.replace(/i/g, "I")
+.replace(/o/g, "O")
+.replace(/u/g, "U");
+return fix;                                             //return final version with capitalized vowels
 };
 
 LetterChanges("hello*3"); //call the function
