1a4b43d970f18266e732755642749c1a50ee4f4b
Connameg <connameg@users.noreply.github.com>
2015-10-20 05:22:34 -0500

    improved Palindrome solution

--- CB_Palindrome.js
+++ CB_Palindrome.js
@@ -1,31 +1,16 @@
 
 //Coderbyte Easy: Palindrome
 
-function Palindrome(str) {
-
-	var splitMe = str.split(" ");				//split string at every space. Becomes array.
-	var join1 = splitMe.join("");				//join array. Any/all spaces should be gone
-
-	var splitMeAgain = join1.split("");			//split string again. 
-	var reverseMe = splitMeAgain.reverse();		//reverse the resulting array
-	var join2 = reverseMe.join("");				//and join it again
-
-	if (join1 === join2) { 						//if both strings are the same,
-		return true;							//return true.
-	} else {									//if not,
-		return false;							//return false.
-	}
-}
-
-Palindrome(readline());  							//call the function with an argument
-
 /*
-The main problem I had writing this was how to make the function 
-return true if the argument is a palindrome, but contains spaces.
-This script separates the argument string at each space, then joins 
-it back without spaces to create join1 for the sake of comparison. 
-If the argument had no spaces to begin with, it won't be affected. 
-Next the script splits, reverses, and rejoins join1. This is stored 
-as join2. An if/else statement checks if join1 & join2 are equal. If
-so, it's a palindrome!
+Objective:
+Write a function that returns true if the string is a palindrome
+and false otherwise.
+ie: "never odd or even", "racecar", and "tacocat" are true
 */
+
+function Palindrome(str) { 
+str = str.replace(/\W/gi, "").toLowerCase();                      //remove spaces in original
+var reverseStr = str.split("").reverse().join("").toLowerCase();  //reverse string
+return str === reverseStr ? true : false;                         //compare    
+} 
+Palindrome("never odd or even"); 
