fb0e8d50753570eebf29cd830fe1e9443dba7219
Connameg <connameg@users.noreply.github.com>
2015-10-20 05:42:14 -0500

    removed unnecessary lines in WordCount

--- CB_WordCount.js
+++ CB_WordCount.js
@@ -1,19 +1,15 @@
 //Coderbyte Easy: Word Count
 
-/*Splitting the string sentence at each space
- means that each word becomes its own item in 
- the resulting array. Thus the length of the 
- array will be the length of the sentence.
- */
+/*
+Objective:
+Write a function that takes a string and returns how many
+words are in the string.
+ie: "Hello World" = 2
+*/
 
 function WordCount(str) { 
- 
-  var splitString = str.split(" ");			//split string with space
-  var sentenceLength = splitString.length;	//get length of resulting array
-  return sentenceLength;
-         
+var splitString = str.split(" ");  //split with spaces to separate words
+return splitString.length;         //return length of resulting array    
 }
-   
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
-WordCount(readline());
+
+WordCount("one 22 three");
