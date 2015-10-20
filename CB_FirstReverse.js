f8211c6c415d275622f756b202a88d86528f8b6a
Connameg <connameg@users.noreply.github.com>
2015-10-20 04:55:00 -0500

    edit FirstReverse solution

--- CB_FirstReverse.js
+++ CB_FirstReverse.js
@@ -1,36 +1,42 @@
 //Coderbyte Easy: First Reverse
 
+/*
+Objective:
+Write a function that takes a string and returns it reversed.
+ie: "Hello World" --> "dlroW olleH"
+*/
 
+
+/*------NO REVERSE METHOD METHOD------*/
+//works without using the array reverse method or loops:
 function FirstReverse(str) { 
-
-  // code goes here  
-  var splitMe = str.split(""); 			//split individual string characters into array
-  var reverseMe = splitMe.reverse(); 	//reverse the array
-  var joinMe = reverseMe.join(""); 		//join the array back into one string
-  return joinMe;
-         
+var reversed = "";
+var split = str.split("");     //split into array
+while (split.length > 0) {     //remove every element from array
+  reversed += split.pop();     //and add it to a variable 
+}                              //pop removes from the end, so it will be reversed
+return reversed;       
 }
-   
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
-FirstReverse(readline());    
+FirstReverse("Hello World");        
 
 
-/*----------LOOP VERSION BELOW----------*/
+/*-------REVERSE METHOD METHOD-------*/
+//uses the array reverse method
+function FirstReverse(str) { 
+var splitMe = str.split("");           //split individual string characters into array
+var reverseMe = splitMe.reverse();     //reverse the array
+var joinMe = reverseMe.join("");       //join the array back into one string
+return joinMe;       
+}
+FirstReverse("Hello World");    
 
-/*This version works too. I wrote it before I studied more about string & array methods.*/
 
-
+/*----------LOOP VERSION----------*/
 function FirstReverse(str) {
-var stringLength = str.length;              //set string length as variable
-msg = "";                                   //set msg to empty string
-for ((i = stringLength); i >= 0; i--) {     //start at end of string, loop backward
-msg += str.charAt(i);                       //add the character with index i to msg
-
+var msg = "";                                 //set msg to empty string
+for (var i = str.length; i >= 0; i--) {       //start at end of string, loop backward
+  msg += str.charAt(i);                       //add the character with index i to msg
 }
-console.log(msg);                           //log msg to console.
-};
-
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
-FirstReverse(readline());
+return msg;
+}
+FirstReverse("Hello World");
